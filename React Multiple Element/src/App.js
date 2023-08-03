19ba143284c40c45e7841be9753703f822b3d3b8
Deepak Yadav <84364713+deep2413@users.noreply.github.com>
2023-08-04 00:39:58 +0530

    Update App.js

--- React Multiple Element/src/App.js
+++ React Multiple Element/src/App.js
@@ -55,32 +55,9 @@ const App = () => {
     ]
 
     return (
-                <div>
-                    <h1>Expense Detail</h1>
-                   <ExpenseItem 
-                       date={expense[0].date} 
-                      title={expense[0].title} 
-                      amount={expense[0].amount}>
-                     </ExpenseItem>
-
-                     <ExpenseItem 
-                       date={expense[1].date} 
-                      title={expense[1].title} 
-                      amount={expense[1].amount}>
-                     </ExpenseItem>
-
-                     <ExpenseItem 
-                       date={expense[2].date} 
-                      title={expense[2].title} 
-                      amount={expense[2].amount}>
-                     </ExpenseItem>
-
-                     <ExpenseItem 
-                       date={expense[3].date} 
-                      title={expense[3].title} 
-                      amount={expense[3].amount}>
-                     </ExpenseItem>
-               </div>
+              <div>
+                <ExpensesComp item={expense}/>
+              </div>
     )      
    
 }
